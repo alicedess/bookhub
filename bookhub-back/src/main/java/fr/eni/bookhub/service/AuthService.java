10f3cf3b90184d30c5d1ce2c0a41f73d92314dd4
Azurly <azerly.hn@gmail.com>
2026-04-10 11:04:06 +0200

    Sécurité & JSON WEB TOKEN configurés v2

--- bookhub-back/src/main/java/fr/eni/bookhub/service/AuthService.java
+++ bookhub-back/src/main/java/fr/eni/bookhub/service/AuthService.java
@@ -1,9 +1,10 @@
 package fr.eni.bookhub.service;
 
-import fr.eni.bookhub.dto.LoginRequest;
+import fr.eni.bookhub.dto.AuthDTO;
 import fr.eni.bookhub.dto.LoginDTO;
 import fr.eni.bookhub.entity.Utilisateur;
 import fr.eni.bookhub.repository.UtilisateurRepository;
+import fr.eni.bookhub.security.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,10 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private UtilisateurRepository utilisateurRepository;
-    private JwtService jwtService;
+    private JwtUtil jwtUtil;
     private PasswordEncoder passwordEncoder;
 
-    public LoginDTO login(LoginRequest loginRequest) {
+    public LoginDTO login(AuthDTO loginRequest) {
         Utilisateur user = utilisateurRepository.findByEmail(loginRequest.getEmail());
 
         if (user == null) {
@@ -31,7 +32,7 @@ public class AuthService {
             throw new RuntimeException("Cet utilisateur a été supprimé");
         }
 
-        String token = jwtService.generateToken(user.getEmail(), user.getId());
+        String token = jwtUtil.generateToken(user.getEmail(), user.getId());
 
         return new LoginDTO(
                 token,
