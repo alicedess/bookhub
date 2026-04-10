7cf1a04fdf61dd5fa868247da0953761d606c10e
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2026-04-10 16:26:30 +0200

    fix : init app

--- bookhub-back/src/main/java/fr/eni/bookhub/storage/FileSystemStorageService.java
+++ bookhub-back/src/main/java/fr/eni/bookhub/storage/FileSystemStorageService.java
@@ -102,7 +102,9 @@ public class FileSystemStorageService implements StorageService {
     @Override
     public void init() {
         try {
-            Files.createDirectory(rootLocation);
+            if (!Files.exists(this.rootLocation)) {
+                Files.createDirectory(rootLocation);
+            }
         } catch (IOException e) {
             throw new StorageException("Could not initialize storage", e);
         }
