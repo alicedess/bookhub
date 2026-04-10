cd17b8a09f7466174eac00e2ca40385ee992afe8
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2026-04-10 11:18:53 +0200

    feat : improve entities

--- bookhub-back/src/main/java/fr/eni/bookhub/entity/Auteur.java
+++ bookhub-back/src/main/java/fr/eni/bookhub/entity/Auteur.java
@@ -7,15 +7,19 @@ import lombok.Data;
 @Table(name = "AUTEUR")
 @Data
 public class Auteur {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
-    private Integer id;
+    private Long id;
 
-    @Column(name = "nom", nullable = false)
+    @Column(name = "nom", nullable = false, length = 100)
     private String nom;
 
-    @Column(name = "prenom", nullable = false)
+    @Column(name = "prenom", nullable = false, length = 100)
     private String prenom;
+
+    /*
+    @OneToMany(mappedBy = "auteur")
+    private Set<Livre> livres = new LinkedHashSet<>();
+    */
 }
---
+++ bookhub-back/src/main/java/fr/eni/bookhub/entity/Evaluation.java
@@ -0,0 +1,47 @@
+package fr.eni.bookhub.entity;
+
+import fr.eni.bookhub.entity.Livre;
+import fr.eni.bookhub.entity.Utilisateur;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Nationalized;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "evaluation")
+@Data
+public class Evaluation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "note")
+    private Integer note;
+
+    @Size(max = 1000)
+    @Nationalized
+    @Column(name = "commentaire", length = 1000)
+    private String commentaire;
+
+    @Column(name = "est_modere")
+    private Boolean estModere;
+
+    @ColumnDefault("getdate()")
+    @Column(name = "date_publication")
+    private Instant datePublication;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_utilisateur", nullable = false)
+    private Utilisateur idUtilisateur;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id_livre", nullable = false)
+    private Livre idLivre;
+}
