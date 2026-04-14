0db18f5034db530741c52fea5d3cfbf83ab74452
Thomas CILES <thomas.ciles2025@campus-eni.fr>
2026-04-14 14:43:00 +0200

    feat(livre) : ajout page liste biblio

---
+++ bookhub-front/src/app/composants/gestion-livre/edition/edition.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edition',
+  imports: [],
+  templateUrl: './edition.html',
+  styleUrl: './edition.css',
+})
+export class Edition {}
