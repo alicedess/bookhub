b0533cac2babc4c9a7a34c85cc5607d6161b6746
Thomas CILES <tciles@webnet.fr>
2026-04-15 23:38:31 +0200

    feat : refactoisation composants + ajout dashboard libraire

--- bookhub-front/src/app/layout/gestion/gestion.ts
+++ bookhub-front/src/app/layout/gestion/gestion.ts
@@ -1,15 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Header } from '../header/header';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-gestion',
-  imports: [
-    Header,
-    RouterLink,
-    RouterOutlet
-  ],
+  imports: [Header, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './gestion.html',
   styleUrl: './gestion.css',
 })
-export class Gestion {}
+export class Gestion {
+  sidebarOuvert = signal(false);
+}
